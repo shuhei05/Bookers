6bd989e7c168a1edd5450c5fae8721243df3d419
shuhei05 <qoo0228y@gmail.com>
2021-04-01 13:14:10 +0000

    28まで終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
      @book = Book.new(book_params)
      if @book.save
-        redirect_to book_path(book.id), notice:"Book was successfully created."
+        redirect_to book_path(@book.id), notice:"Book was successfully created."
      else
        @books = Book.all
 
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if
       @book.update(book_params)
-    redirect_to book_path(book.id), notice:"Book was successfully updated."
+    redirect_to book_path(@book.id), notice:"Book was successfully updated."
     else render :edit
 
     end
